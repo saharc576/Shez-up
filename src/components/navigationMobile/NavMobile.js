533ef3079c264bd69b149c7fb2f876798b5921e3
sahar <sahar576cohen@gmail.com>
2021-07-29 13:32:55 +0300

    done layout

--- src/components/navigationMobile/NavMobile.js
+++ src/components/navigationMobile/NavMobile.js
@@ -13,15 +13,15 @@ const NavMobile = ({loggedIn}) => {
     }
     return (
         <div>
-            <div className={
-                NavBarStyle.topnav
-            }>
-                <Menu onClick={menuHandler} className={NavBarStyle.menu}/>
-                <div >
+            <img src="https://firebasestorage.googleapis.com/v0/b/shez-up.appspot.com/o/logo.png?alt=media&token=ddd30c5a-e91b-4843-bba7-a907c5c1b33a" 
+                className={NavBarStyle.icon}/> 
+            <section style={{position: 'fixed' ,width:'100%',height:'3rem' ,backgroundColor:'#161515'}}>
+                <Menu color='white' onClick={menuHandler} className={NavBarStyle.menu}/>
+            </section>
+            <div className={NavBarStyle.topnav}>
                     {isOpenMenu &&<NavLink to='/Login'>News</NavLink>}
                     {isOpenMenu &&<NavLink to='/Home'>Home</NavLink>}
                     {isOpenMenu &&<NavLink to='/Gallery'>News</NavLink>}
-                </div>
             </div>
         </div>
     );
